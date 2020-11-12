7f49b2e09cbdd5311bbb1956eb6c177b25285773
sdelahoz <sdelahoz@nativapps.co>
2020-11-12 18:06:00 -0500

    language i18n

--- src/api/languages.js
+++ src/api/languages.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import { api } from './api';
 
-export const getLanguages = () => {
+export const getLanguages = (lang) => {
     const URL = `/languages/`;
+    if(lang=="ES"){
+        lang="es"
+    }else{
+        lang="en"
+    }
     return api(URL, {
         method: 'GET',
         headers: {
             'content-type': 'application/json'
         },
+        params:{
+            lang: lang
+        }
     })
     .then(response => response.data)
     .catch(error => {
