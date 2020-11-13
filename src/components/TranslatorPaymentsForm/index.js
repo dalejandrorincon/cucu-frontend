97b64b2738160e9758ade306ee267be16ca0ec7d
sdelahoz <sdelahoz@nativapps.co>
2020-11-12 20:49:11 -0500

    new window links

--- src/components/TranslatorPaymentsForm/index.js
+++ src/components/TranslatorPaymentsForm/index.js
@@ -171,8 +171,8 @@ export default function TranslatorProfileForm() {
             <Title>{t('bank-info.personal-information')}</Title>
 
             <Alert variant="primary">
-                {t('bank-info.payoneer-label-1')}<a href="https://stripe.com/global">{t('bank-info.payoneer-link')}</a>{t('bank-info.payoneer-label-2')}
-                <p>{t('bank-info.payoneer-account-label')}<a href="https://www.payoneer.com/resources/payoneer-account/">{t('bank-info.payoneer-account-link')}</a></p>
+                {t('bank-info.payoneer-label-1')}<a target="_blank" href="https://stripe.com/global">{t('bank-info.payoneer-link')}</a>{t('bank-info.payoneer-label-2')}
+                <p>{t('bank-info.payoneer-account-label')}<a target="_blank" href="https://www.payoneer.com/resources/payoneer-account/">{t('bank-info.payoneer-account-link')}</a></p>
             </Alert>
 
             <Form onSubmit={formik.handleSubmit}>
